f82f0104abaef3abb1d5529b6739a8dc3ab63380
P4 <umeaansa555@ed.amdsb.ca>
2021-11-23 12:37:46 -0500

    string angle

--- Form1.cs
+++ Form1.cs
@@ -29,9 +29,21 @@ namespace LogoGraphic
 
             g.Clear(Color.Black);
 
-            g.FillRectangle(redBrush, 30, 30, 100, 200);
-            g.FillRectangle(blueBrush, 30, 30, 100, 200);
-            g.DrawString("Domino's Pizza", drawFont, whiteBrush, 50, 40);
+            //g.RotateTransform(45);
+            g.FillRectangle(redBrush, 150, 30, 50, 50);
+            //g.RotateTransform(45);
+            g.FillRectangle(redBrush, 130, 90, 50, 50);
+
+            
+            //g.TranslateTransform(60, 60);
+            g.FillRectangle(blueBrush, 0, 0, 50, 100);
+
+            g.FillEllipse(whiteBrush, 50, 25, 25, 25);
+            g.FillEllipse(whiteBrush, 70, 25, 25, 25);
+
+            g.TranslateTransform(300, 300);
+            g.RotateTransform(-45);
+            g.DrawString("Domino's Pizza", drawFont, whiteBrush, 0, 0);
         }
     }
 }
